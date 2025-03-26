256cab84b9232a15d24fcfa1e4fce32e2f07ae2a
rovycutie <157572574+rovycutie@users.noreply.github.com>
2025-03-26 10:35:06 +0800

    Revert "Activity 2"

--- dashboard_project/dashboard_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def dashboard_view(request):
-    data = [
-        {"title": "Users", "count": 150},
-        {"title": "Orders", "count": 320},
-        {"title": "Revenue", "count": "12450"},
-    ]
-    return render(request, "dashboard.html", {"data": data})
